d8bdebffe3766f6eecc2dbeef7f7511c156fb264
xiluna <xilaluna2@gmail.com>
2020-11-06 01:00:49 -0800

    Update BankAccount.py

---
+++ BankAccount.py
@@ -0,0 +1,10 @@
+class BankAccount:
+    """
+    initializes a users bank account and actions.
+    """
+
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
